9dec68849fd21f4ec96b82c29fa0b9365bed1a44
jose-oyuko <oyukojoseph13@gmail.com>
2023-04-11 10:21:02 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,11 +5,25 @@
  * @n: int
  * Return: 0 or 1
  */
-int check_prime(int n, int _2)
 
 int is_prime_number(int n)
 {
-	return (check_prime(n, 2));
+	if (n <2){
+		return (0);
+	}
+	if (n == 2){
+		return (1);
+	}
+	if (n % 2 == 0){
+		return (0);
+	}
+	for (int i = 3; i*i <= n; i+= 2){
+		if (n % i == 0){
+			return (0);
+		}
+	}
+	return (1);
+
 }
 
 /**
@@ -19,13 +33,4 @@ int is_prime_number(int n)
  * Return: int
  */
 
-int check_prime(int n, int _2)
-{
 
-	if (_2 >= n && n > 1)
-		return (1);
-	else if (n % _2 == 0 || n <= 1)
-		return (0);
-	else
-		return (check_prime(n, _2 + 1));
-}
